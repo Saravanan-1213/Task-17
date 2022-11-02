8cda13737bf3a65c39c78599f005bc6994ef663f
SaraVanan S <112925088+Saravanan-1213@users.noreply.github.com>
2022-11-02 07:09:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+
+var count=10;
+
+setTimeout(function(){
+    count --;
+
+    if(count>=0){
+        id=document.getElementById("main");
+        id.innerHTML=count;
+    }
+    
+    setTimeout(function(){
+    
+        count --;
+    
+        if(count>=0){
+            id=document.getElementById("main");
+            id.innerHTML=count;
+    
+        }
+        setTimeout(function(){
+    
+            count --;
+        
+            if(count>=0){
+                id=document.getElementById("main");
+                id.innerHTML=count;
+        
+            }
+            setTimeout(function(){
+    
+                count --;
+            
+                if(count>=0){
+                    id=document.getElementById("main");
+                    id.innerHTML=count;
+            
+                }
+                setTimeout(function(){
+    
+                    count --;
+                
+                    if(count>=0){
+                        id=document.getElementById("main");
+                        id.innerHTML=count;
+                
+                    }
+                    setTimeout(function(){
+    
+                        count --;
+                    
+                        if(count>=0){
+                            id=document.getElementById("main");
+                            id.innerHTML=count;
+                    
+                        }
+                        setTimeout(function(){
+    
+                            count --;
+                        
+                            if(count>=0){
+                                id=document.getElementById("main");
+                                id.innerHTML=count;
+                        
+                            }
+                            setTimeout(function(){
+    
+                                count --;
+                            
+                                if(count>=0){
+                                    id=document.getElementById("main");
+                                    id.innerHTML=count;
+                            
+                                }
+                                setTimeout(function(){
+    
+                                    count --;
+                                
+                                    if(count>=0){
+                                        id=document.getElementById("main");
+                                        id.innerHTML=count;
+                                
+                                    }
+                                    setTimeout(function(){
+    
+                                        count --;
+                                    
+                                        if(count>=0){
+                                            id=document.getElementById("main");
+                                            id.innerHTML=count;
+                                    
+                                        }
+                                        if(count===0){
+                                                    id.innerHTML="HAPPY INDEPENDENCE DAY"
+                                            
+                                                }
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+   },1000)
+
